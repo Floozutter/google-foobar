0fcaaf9e71bda84b7756cdddae5b04b0caf20bde
Floozutter <floozutter@gmail.com>
2020-03-10 23:00:58 -0700

    Improve naming and comments

--- level3-find-the-access-codes/solution.py
+++ level3-find-the-access-codes/solution.py
@@ -4,16 +4,16 @@ def solution(l):
     # Build dictionary of factor relationships:
     #   - Key: An index to a multiple.
     #   - Value: A set of indices to factors of the multiple.
-    factor_indices = defaultdict(set)
+    mult2fact = defaultdict(set)
     for i in range(len(l)-1):
         for j in range(i+1, len(l)):
             if l[j] % l[i] == 0:
-                factor_indices[j].add(i)
+                mult2fact[j].add(i)
     # Count the number of factors of factors, which imply lucky triples.
     luckies = 0
-    for m in factor_indices:             # m points to a multiple
-        for f in factor_indices[m]:      # f points to a factor of m
-            if f in factor_indices:      # check if f is also a multiple
-                ffs = factor_indices[f]  # ffs is a set of factors of factors
-                luckies += len(ffs)      # add the number of factor factors
+    for m in mult2fact:              # m is a multiple
+        for f in mult2fact[m]:       # f is a factor of m
+            if f in mult2fact:       # check if f is also a multiple
+                ffs = mult2fact[f]   # ffs is the set of factors of f
+                luckies += len(ffs)  # increment the number of factor factors
     return luckies
