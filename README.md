917b0844b09d25b72438342fa1c75522ef54dba6
Floozutter <floozutter@gmail.com>
2020-03-12 01:41:58 -0700

    Add solution for Level 3: PtBE

--- README.md
+++ README.md
@@ -2,10 +2,11 @@
 My solutions for the Google FooBar Challenge
 
 ## Times
-| Level | Name of Challenge         |  Time (mm:ss) |
-| ----: | ------------------------- | ------------: |
-|     1 | Minion Labor Shifts       |         10:01 |
-|     2 | En Route Salute           |          7:19 |
-|     2 | Elevator Maintenance      |         13:38 |
-|     3 | Find the Access Codes     |       2:05:35 |
-|     3 | Fuel Injection Perfection |       2:35:38 |
+| Level | Name of Challenge           |  Time (mm:ss) |
+| ----: | --------------------------- | ------------: |
+|     1 | Minion Labor Shifts         |         10:01 |
+|     2 | En Route Salute             |          7:19 |
+|     2 | Elevator Maintenance        |         13:38 |
+|     3 | Find the Access Codes       |       2:05:35 |
+|     3 | Fuel Injection Perfection   |       2:35:38 |
+|     3 | Prepare the Bunnies' Escape |       4:32:30 |
---
+++ level3-prepare-the-bunnies-escape/solution.py
@@ -0,0 +1,147 @@
+import heapq
+from functools import total_ordering
+
+# Global constants.
+CLEAR = 0
+WALL = 1
+DIRECTIONS = (
+    ( 0,  1),  # RIGHT
+    (-1,  0),  # UP
+    ( 0, -1),  # LEFT
+    ( 1,  0)   # DOWN
+)
+
+
+# Classes for maze solving.
+@total_ordering
+class Cell:
+    """
+    Represents a single cell in a BunnyMap.
+    Provides methods and properties for A*.
+    """
+    def __init__(self, state, row, col, h):
+        """Initializes a Cell."""
+        self.state = state
+        self.row = row
+        self.col = col
+        self.heuristic = h  # Estimated cost to ending Cell.
+        self.gap = None     # Cost from the starting Cell.
+        self.parent = None  # Adjacent Cell closest to the starting Cell.
+    def get_coord(self):
+        """Returns the Cell's coordinates as a Tuple."""
+        return self.row, self.col
+    def get_fullcost(self):
+        """Returns the estimated sum cost from the start to the end."""
+        if self.gap is not None:
+            return self.heuristic + self.gap
+        return None
+    def set_parent(self, parent):
+        """
+        Sets an adjacent Cell as this Cell's parent.
+        Updates this Cell's gap (cost from the start Cell).
+        """
+        self.gap = parent.gap + 1
+        self.parent = parent
+    def __eq__(self, other):
+        """Equality comparison function for priority queue ordering."""
+        return self.get_fullcost() == other.get_fullcost()
+    def __lt__(self, other):
+        """Less than comparison function for priority queue ordering."""
+        return self.get_fullcost() < other.get_fullcost()
+
+class BunnyMap:
+    """
+    Represents a map of a location in Lambda's space station.
+    Provides methods for solving the maze using the A* search algorithm.
+    """
+    def __init__(self, mat):
+        """Initializes a BunnyMap from a List of Lists of states."""
+        # Set map dimensions.
+        self.height = len(mat)
+        self.width  = len(mat[0])
+        # Populate map with cells.
+        self.cells = [
+            [
+                Cell(value, i, j, abs(self.height-i) + abs(self.width-j))
+                for j, value in enumerate(row)
+            ]
+            for i, row in enumerate(mat)
+        ]
+    def get_cell(self, row, col):
+        """Returns a reference to the Cell at the given coordinate."""
+        if (0 <= row < self.height) and (0 <= col < self.width):
+            return self.cells[row][col]
+        return None
+    def adjacent_cells(self, row, col):
+        """Returns every valid Cell adjacent to the given coordinate."""
+        maybecells = [self.get_cell(row+i, col+j) for i, j in DIRECTIONS]
+        return [cell for cell in maybecells if cell is not None]
+    def aStarPath(self):
+        """
+        Runs the A* search algorithm over the BunnyMap.
+        Returns a path (list of coordinates) from the start to the end.
+        """
+        solved = False
+        # Set start and end cells.
+        start = self.get_cell(0, 0)
+        end   = self.get_cell(self.height-1, self.width-1)
+        # Set start's gap distance to 0.
+        start.gap = 0
+        # Initialize the open and closed structures.
+        openQueue = [start]  # min-heap priority queue
+        closedSet = set()
+        # Start consuming openQueue.
+        while openQueue:
+            # Take the cell with the lowest fullcost.
+            cell = heapq.heappop(openQueue)
+            closedSet.add(cell.get_coord())
+            # Check if end reached.
+            if cell.get_coord() == end.get_coord():
+                solved = True
+                break
+            # Iterate over adjacent cells.
+            for adj in self.adjacent_cells(cell.row, cell.col):
+                if adj.get_coord() not in closedSet and adj.state == CLEAR:
+                    # Check whether adj has never been visited.
+                    if adj not in openQueue:
+                        adj.set_parent(cell)
+                        heapq.heappush(openQueue, adj)
+                    else:  # adj has been visited before.
+                        # Check if current cell is a better parent.
+                        if adj.gap > cell.gap + 1:
+                            adj.set_parent(cell)
+        # Check if solution exists.
+        if not solved:
+            return None
+        # From end, trace parents to get the path back.
+        path = []
+        cell = end
+        while not (cell.get_coord() == start.get_coord()):
+            path.append(cell.get_coord())
+            cell = cell.parent
+        path.append(cell.get_coord())
+        return path[::-1]
+    def shortest_pathlength(self):
+        """Returns the length of the shortest path from start to end."""
+        path = self.aStarPath()
+        if path is not None:
+            return len(path)
+        return None
+
+
+# Solution function.
+def solution(mat):
+    """Returns the shortest path length by A*ing over every possible map."""
+    pathlengths = []
+    # Iterate over every single possible map. (Each is missing one wall.)
+    wall_coords = []
+    for i, row in enumerate(mat):
+        for j, value in enumerate(row):
+            if value == WALL:
+                wall_coords.append( (i, j) )
+    dewalled_mat = [list(row) for row in mat]
+    for i, j in wall_coords:
+        dewalled_mat[i][j] = CLEAR
+        pathlengths.append(BunnyMap(dewalled_mat).shortest_pathlength())
+        dewalled_mat[i][j] = WALL
+    return min(filter(lambda l: l is not None, pathlengths))
